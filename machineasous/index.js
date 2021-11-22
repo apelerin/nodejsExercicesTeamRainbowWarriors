8a68a200d79c8478acbda66361a5c10b6709031b
Axel Pelerin <arcanynka@gmail.com>
2021-11-22 13:47:41 +0100

    Fix proba algo

--- machineasous/index.js
+++ machineasous/index.js
@@ -1,17 +1,30 @@
 const crypto = require("crypto")
 const symbols = ["💘", "🌭", "🍺", "💸", "⛄", "🍆", "🍑", "😈"]
+const allEqual = arr => arr.every(val => val === arr[0]);
 
-function probaCalc(nbrSymbols, nbrResult) {
-    return (Math.pow(1 / nbrSymbols, nbrResult)) * 100
+function probaCalc(nbrBonTirage, nbrTirage) {
+    return (nbrBonTirage / nbrTirage) * 100
 }
 
-const rds = []
-for (let i = 0; i <= 2; i++) {
-    rds.push(crypto.randomInt(0, 7))
+let rds = []
+let nbrGoodTirage = 0
+let nbrTirage = 0
+
+while(nbrTirage < 10000000) {
+    for (let i = 0; i <= 2; i++) {
+        rds.push(crypto.randomInt(0, 8))
+    }
+
+    //rds.forEach((element) => {
+    //    process.stdout.write(symbols[element] + ' ')
+    //})
+
+    if (allEqual(rds)) {
+        nbrGoodTirage++
+    }
+
+    rds = []
+    nbrTirage++
 }
-
-rds.forEach((element) => {
-    process.stdout.write(symbols[element] + ' ')
-})
-
-console.log('\n' + probaCalc(symbols.length, rds.length))
+console.log(nbrGoodTirage)
+console.log(probaCalc(nbrGoodTirage, nbrTirage))
